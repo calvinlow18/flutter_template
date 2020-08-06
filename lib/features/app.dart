b04e66e98f3245009d8fc8a4ee5e0f7f4f998a62
Calvin Low Khai Wynn <calvin.low.kw@fusionex.local>
2020-08-06 11:33:05 +0800

    Updated

--- lib/features/app.dart
+++ lib/features/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
